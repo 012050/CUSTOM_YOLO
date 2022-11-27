0638e8934a094638574afcbb6a97b8062a3c9a41
012050 <beomjunchoe@naver.com>
2022-11-27 13:22:45 +0900

    옵션 추가

--- extraction.py
+++ extraction.py
@@ -1,3 +1,4 @@
+import argparse
 import fiftyone as fo
 import fiftyone.zoo as foz
 import os
@@ -5,15 +6,20 @@ from glob import glob
 from sklearn.model_selection import train_test_split
 import yaml
 
-the_number = 6000
-text_rate = 0.05
+parser = argparse.ArgumentParser()
+parser.add_argument('--number', type=int, default=100, help='setting the Size of the Dataset')
+parser.add_argument('--rate', type=float, default=0.2, help='setting test rate')
+args = parser.parse_args()
+
+print("dataset size:", args.number, "\ntest rate:", args.rate)
+print("test dataset size is", int(args.number * args.rate))
 
 dataset = foz.load_zoo_dataset(
     "open-images-v6",
     split="train",
     lable_types=["detections"],
     classes=["Fish"],
-    max_samples=the_number,
+    max_samples=args.number,
     )
 dataset.export(export_dir= os.getcwd(),dataset_type=fo.types.YOLOv5Dataset,)
 
@@ -25,7 +31,7 @@ img_list = glob(path1)
 print(len(img_list))
 
 # -----------------------------------------------------------
-train_img_list, val_img_list = train_test_split(img_list, test_size=text_rate, random_state=len(img_list))
+train_img_list, val_img_list = train_test_split(img_list, test_size=args.rate, random_state=len(img_list))
 print(len(train_img_list), len(val_img_list))
 
 # -----------------------------------------------------------
